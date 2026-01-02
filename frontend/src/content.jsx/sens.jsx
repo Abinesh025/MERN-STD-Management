7a39aca6cf9f7b797e05615c1162e99e62adf6a0
Abinesh <abi18official.com>
2026-01-02 15:18:07 +0530

    mongoUrl  commit

--- frontend/src/content.jsx/sens.jsx
+++ frontend/src/content.jsx/sens.jsx
@@ -1,1 +1,2 @@
-export const baseUrl = "https://mern-std-management.onrender.com"
+// export const baseUrl = "https://mern-std-management.onrender.com"
+export const baseUrl = "https://mern-std-management-1.onrender.com"
